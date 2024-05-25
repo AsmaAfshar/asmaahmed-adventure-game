013247e0cfe9b39d85ac3ac58c0175f80cf54436
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2024-05-25 22:01:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+//for player 
+class Player {
+    name;
+    health = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    decreaseHealth() {
+        this.health -= 20;
+    }
+    increaseHealth() {
+        this.health = 100;
+    }
+}
+// for enemy
+class Enemy {
+    name;
+    health = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    decreaseHealth() {
+        this.health -= 20;
+    }
+    increaseHealth() {
+        this.health = 100;
+    }
+}
+//STEP 2
+// ========================== player object =====================
+async function main() {
+    const { playerName } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "playerName",
+            message: "Enter Your Player Name:"
+        }
+    ]);
+    // ====================== enemy object =======================
+    const { enemyType } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "enemyType",
+            choices: ["Alien", "Witch", "Zombie"],
+            message: "Select the Enemy you want to fight with him:"
+        }
+    ]);
+    // step 3 battle  field
+    const player = new Player(playerName);
+    const enemy = new Enemy(enemyType);
+    console.log(`${enemy.name} v/s ${Player.name}`);
+    // step 4 action
+    do {
+        const { action } = await inquirer.prompt([
+            // action object
+            {
+                type: "list",
+                name: "action",
+                choices: ["Attack", "Defend", "Range Target", "Run"],
+                message: "choosse the attact type to perform action!"
+            }
+        ]);
+        switch (action) {
+            case "Attack":
+                const randomNum = Math.random();
+                if (randomNum > 0.5) {
+                    player.decreaseHealth();
+                    console.log(`${player.name} health: ${player.health}`);
+                    console.log(`${enemy.name} health: ${enemy.health}`);
+                    if (player.health <= 0) {
+                        console.log("Ooooppsss You loss! try again");
+                        return;
+                    }
+                    else {
+                        // enemy health decrease
+                        enemy.decreaseHealth();
+                        enemy.decreaseHealth();
+                        console.log(`${player.name} health: ${player.health}`);
+                        console.log(`${enemy.name} health: ${enemy.health}`);
+                        if (player.health <= 0) {
+                            console.log("Congratulations! You Won");
+                            return;
+                        }
+                    }
+                    break;
+                }
+        }
+    } while (true);
+}
+main();
